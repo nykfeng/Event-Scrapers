907965eabfef3c057533a22505a6154344ae4894
Lifeng Huang <nykfengcode@gmail.com>
2022-11-19 01:32:59 -0500

    finishe scraper

--- app.js
+++ app.js
@@ -24,14 +24,46 @@ async function main() {
     for (const td of tableData) {
       const companyName = td.querySelector(".exhibitorName").textContent;
       const booth = td.querySelector(".boothLabel").textContent;
-      td.querySelector(".boothLabel a").click();
 
+      td.querySelector(".boothLabel a").click();
+      await new Promise((r) => setTimeout(r, 1000));
+
+      td.querySelector("a.exhibitorName").click();
       await new Promise((r) => setTimeout(r, 3000));
 
-      //   await page.waitForSelector(".leaflet-popup-content .fpPopupBoothSize");
-      const boothSize = document.querySelector(
-        ".leaflet-popup-content .fpPopupBoothSize"
-      ).textContent;
+      const iframeEl = document.querySelector(".modal-content iframe")
+        .contentWindow.document;
+      const contactInfoEl = iframeEl.querySelector(".panel-body");
+      let cityName =
+        contactInfoEl
+          .querySelector(".BoothContactCity")
+          ?.textContent.replaceAll(/\n/g, "") ?? "";
+      let stateName =
+        contactInfoEl
+          .querySelector(".BoothContactState")
+          ?.textContent.replaceAll(/\n/g, "") ?? "";
+      let countryName =
+        contactInfoEl
+          .querySelector(".BoothContactCountry")
+          ?.textContent.replaceAll(/\n/g, "") ?? "";
+      let url =
+        contactInfoEl.querySelector(".BoothContactUrl")?.textContent ?? "";
+
+      const descriptionContainerEl = iframeEl.querySelector("#eboothContainer");
+      const allParagraphEls = descriptionContainerEl.querySelectorAll("p");
+
+      let description = "";
+      if (allParagraphEls) {
+        allParagraphEls.forEach((p) => {
+          if (p.textContent) description += p.textContent.replaceAll(/\n/g, "");
+        });
+      }
+
+      description.replaceAll(/\t/g, "");
+
+      let boothSize =
+        document.querySelector(".leaflet-popup-content .fpPopupBoothSize")
+          .textContent || "Can't get size";
 
       if (boothSize !== "") {
         boothSize = boothSize.includes("Size: ")
@@ -43,7 +75,14 @@ async function main() {
         companyName,
         booth,
         boothSize,
+        description,
+        cityName,
+        stateName,
+        countryName,
+        url,
       });
+
+      if (companyName === "Clean Air Group") break;
     }
 
     // });
